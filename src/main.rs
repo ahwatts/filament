11d924360749634f51a0ba61c90d01c762da7bc3
Andrew Watts <ahwatts@gmail.com>
2015-06-17 00:45:24 -0400

    Refactoring command-line args, and started the get_paths command.

--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,20 @@ type CommandArgs = HashMap<String, Vec<String>>;
 type TrackerResult = Result<String, String>;
 
 fn main() {
-    let mut mysql_user = "mogile".to_string();
-    let mut mysql_pass = "".to_string();
+    let mut opts: Options = Default::default();
+    opts = opts.parse_args();
+    let listen_addr: &str = &opts.listen;
+    let listener = TcpListener::bind(listen_addr).unwrap();
 
-    {
-        let mut parser = ArgumentParser::new();
-        parser.set_description("A partial clone for the MogileFS tracker daemon.");
-        parser.refer(&mut mysql_user).add_option(&[ "-u", "--user" ], Store, "The username for the MySQL connection.");
-        parser.refer(&mut mysql_pass).add_option(&[ "-p", "--password" ], Store, "The password for the MySQL connection.");
-        parser.parse_args_or_exit();
-    }
-
-    let listener = TcpListener::bind("127.0.0.1:7002").unwrap();
     let mysql_opts = MyOpts {
-        user: Some(mysql_user.clone()),
-        pass: Some(mysql_pass.clone()),
-        db_name: Some("mogilefs".to_string()),
+        tcp_addr: Some(opts.mysql_host),
+        tcp_port: opts.mysql_port as u16,
+        user: Some(opts.mysql_user),
+        pass: Some(opts.mysql_pass),
+        db_name: Some(opts.mysql_db),
         ..Default::default()
     };
+
     let db_pool = MyPool::new(mysql_opts).unwrap_or_else(|e| {
         panic!("Error connecting to MySQL: {}", e);
     });
@@ -50,6 +46,47 @@ fn main() {
                 panic!("Connection failed: {:?}", e);
             },
         }
+    }
+}
+
+#[derive(Debug)]
+struct Options {
+    listen: String,
+    mysql_host: String,
+    mysql_port: i32,
+    mysql_user: String,
+    mysql_pass: String,
+    mysql_db: String,
+}
+
+impl Default for Options {
+    fn default() -> Options {
+        Options {
+            listen: "0.0.0.0:7001".to_string(),
+            mysql_host: "127.0.0.1".to_string(),
+            mysql_port: 3306,
+            mysql_user: "mogile".to_string(),
+            mysql_pass: "".to_string(),
+            mysql_db: "mogilefs".to_string(),
+        }
+    }
+}
+
+impl Options {
+    fn parse_args(mut self) -> Options {
+        {
+            let mut parser = ArgumentParser::new();
+            parser.set_description("A partial clone for the MogileFS tracker daemon.");
+            parser.refer(&mut self.listen)    .add_option(&[ "-l", "--listen"   ], Store, "The host:port for the tracker to listen on.");
+            parser.refer(&mut self.mysql_host).add_option(&[ "-H", "--db-host"  ], Store, "The MySQL server address.");
+            parser.refer(&mut self.mysql_port).add_option(&[ "-P", "--db-port"  ], Store, "The MySQL server port.");
+            parser.refer(&mut self.mysql_user).add_option(&[ "-u", "--user"     ], Store, "The username for the MySQL connection.");
+            parser.refer(&mut self.mysql_pass).add_option(&[ "-p", "--password" ], Store, "The password for the MySQL connection.");
+            parser.refer(&mut self.mysql_db)  .add_option(&[ "-D", "--db-name"  ], Store, "The MySQL database name.");
+            parser.parse_args_or_exit();
+        }
+
+        self
     }
 }
 
@@ -105,19 +142,15 @@ impl Handler {
 
         match command {
             Some("list_keys") => self.list_keys_command(&parse_query_string(args.unwrap().as_bytes())),
-            _ => {
-                Err("because f*** you, that's why.".to_string())
-            }
+            Some("get_paths") => self.get_paths_command(&parse_query_string(args.unwrap().as_bytes())),
+            _ => Err("because f*** you, that's why.".to_string()),
         }
     }
 
     fn list_keys_command(&self, args: &CommandArgs) -> TrackerResult {
-        println!("args = {:?}", args);
+        // println!("args = {:?}", args);
 
-        let domain_name   = try!(args.get("domain").and_then(|v| v.first()).ok_or("ERR no_domain No+domain+provided"));
-        let domain_id_opt = try!(self.store.get_domain_id(domain_name).map_err(|e| format!("ERR {}", e.to_error_string())));
-        let domain_id     = try!(domain_id_opt.ok_or("ERR unreg_domain Domain+name+invalid/not+found"));
-
+        let domain_id = try!(get_domain_id_from_args(args, &self.store));
         let prefix = args.get("prefix").and_then(|v| v.first());
         let after = args.get("after").and_then(|v| v.first());
         let limit = args.get("limit").and_then(|v| v.first())
@@ -125,7 +158,7 @@ impl Handler {
             .and_then(|d| clamp(d, 0, 1000))
             .unwrap_or(1000);
 
-        println!("domain_id = {:?} prefix = {:?} after = {:?} limit = {:?}", domain_id, prefix, after, limit);
+        // println!("domain_id = {:?} prefix = {:?} after = {:?} limit = {:?}", domain_id, prefix, after, limit);
 
         if prefix.is_some() && after.is_some() {
             let real_after: &str = after.unwrap();
@@ -142,6 +175,25 @@ impl Handler {
 
         Ok(format!("OK {}", form_urlencoded::serialize(returned_values)))
     }
+
+    fn get_paths_command(&self, args: &CommandArgs) -> TrackerResult {
+        println!("args = {:?}", args);
+
+        let domain_id = try!(get_domain_id_from_args(args, &self.store));
+        let key = try!(args.get("key").and_then(|v| v.first()).ok_or("ERR no_key No+key+provided"));
+        let path_count = args.get("pathcount").and_then(|v| v.first())
+            .and_then(|s| i32::from_str_radix(s, 10).ok())
+            .and_then(|d| clamp(d, 2, 10))
+            .unwrap_or(2);
+
+        Err(format!("ERR no_domain No+domain+provided"))
+    }
+}
+
+fn get_domain_id_from_args(args: &CommandArgs, store: &store::Store) -> Result<i32, String> {
+    let domain_name   = try!(args.get("domain").and_then(|v| v.first()).ok_or("ERR no_domain No+domain+provided"));
+    let domain_id_opt = try!(store.get_domain_id(domain_name).map_err(|e| format!("ERR {}", e.to_error_string())));
+    domain_id_opt.ok_or("ERR unreg_domain Domain+name+invalid/not+found".to_string())
 }
 
 fn parse_query_string(query_string: &[u8]) -> CommandArgs {
