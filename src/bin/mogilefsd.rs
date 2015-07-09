3c73f3e1eb690b9ec19fb6ec9351ce0783fbe641
Andrew Watts <ahwatts@gmail.com>
2015-07-09 17:26:40 -0400

    Move the Handler stuff to a "tracker" module in the "mogilefsd" lib.

--- src/bin/mogilefsd.rs
+++ src/bin/mogilefsd.rs
@@ -1,11 +1,13 @@
 extern crate argparse;
+extern crate mogilefsd;
 
 use argparse::ArgumentParser;
 use std::collections::HashMap;
 use std::default::Default;
-use std::io::{BufRead, BufReader, Write};
-use std::net::{TcpListener, TcpStream, Ipv4Addr};
+use std::net::{TcpListener, Ipv4Addr};
 use std::thread;
+use mogilefsd::tracker;
+use std::sync::Arc;
 
 type CommandArgs = HashMap<String, Vec<String>>;
 type TrackerResult = Result<String, String>;
@@ -14,11 +16,13 @@ fn main() {
     let mut opts: Options = Default::default();
     opts.parser().parse_args_or_exit();
     let listener = TcpListener::bind((opts.listen_ip, opts.listen_port)).unwrap();
+    let handler = Arc::new(tracker::Handler::new());
 
     for stream_result in listener.incoming() {
+        let local_handler = handler.clone();
         match stream_result {
             Ok(stream) => {
-                thread::spawn(move|| handle_client(stream));
+                thread::spawn(move|| local_handler.handle(stream));
             },
             Err(e) => {
                 panic!("Connection failed: {:?}", e);
@@ -61,53 +65,7 @@ impl Options {
     }
 }
 
-fn handle_client(stream: TcpStream) {
-    let mut handler = Handler;
-    handler.handle(stream);
-}
-
-struct Handler;
-
-impl Handler {
-    fn handle(&mut self, mut stream: TcpStream) {
-        println!("Connection received: local = {:?} remote = {:?}",
-                 stream.local_addr(), stream.peer_addr());
-        let reader = BufReader::new(stream.try_clone().unwrap());
-
-        for line_result in reader.lines() {
-            match line_result {
-                Ok(line) => {
-                    println!("request  = {:?}", line);
-                    let response = self.dispatch_command(&line.trim_right());
-                    println!("response = {:?}", response);
-
-                    // Okay, both arms here are the same, but maybe they
-                    // won't be in the future?
-                    match response {
-                        Ok(response_str) => {
-                            write!(stream, "{}\r\n", response_str)
-                                .unwrap_or_else(|e| println!("Error writing successful response: {:?}", e));
-                        },
-                        Err(err_str) => {
-                            write!(stream, "{}\r\n", err_str)
-                                .unwrap_or_else(|e| println!("Error writing error response: {:?}", e));
-                        }
-                    }
-                },
-                Err(e) => {
-                    println!("Error with connection: {:?}", e);
-                    break;
-                }
-            }
-        }
-    }
-
-    fn dispatch_command(&mut self, line: &str) -> TrackerResult {
-        let mut toks = line.split(" ");
-        let command = toks.next();
-
-        match command {
-            _ => Err("because f*** you, that's why.".to_string()),
-        }
-    }
-}
+// fn handle_client(stream: TcpStream) {
+//     let mut handler = tracker::Handler::new();
+//     handler.handle(stream);
+// }
