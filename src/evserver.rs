ab21f5eb70c2fe1f803a650bbc71faab4a382fa4
Andrew Watts <ahwatts@gmail.com>
2015-07-14 22:17:37 -0400

    Close down connections when the client disconnects.

--- src/evserver.rs
+++ src/evserver.rs
@@ -1,13 +1,13 @@
 use mio::buf::{Buf, MutBuf, RingBuf};
 use mio::tcp::{self, TcpListener, TcpStream};
-use mio::{EventLoop, Handler, Interest, NonBlock, PollOpt, ReadHint, Socket, Token, TryRead, TryWrite};
+use mio::{EventLoop, Handler, Interest, NonBlock, NotifyError, PollOpt, ReadHint, Socket, Token, TryRead, TryWrite};
 use std::collections::HashMap;
 use std::error;
 use std::fmt::{self, Display, Formatter};
 use std::io::{self, Cursor};
 use std::net::{Shutdown, ToSocketAddrs};
-use std::result;
 use std::rc::Rc;
+use std::result;
 use super::ctrlc::CtrlC;
 use super::tracker::Tracker;
 
@@ -32,7 +32,7 @@ impl Server {
         // register a handler for ctrl+c.
         let notify_channel = self.event_loop.channel();
         CtrlC::set_handler(move|| {
-            notify_channel.send(()).unwrap_or_else(|e| {
+            notify_channel.send(Notification::shutdown()).unwrap_or_else(|e| {
                 println!("Error notifying event loop of SIGINT: {:?}", e);
             });
         });
@@ -80,22 +80,45 @@ impl ServerHandler {
         self.conns.insert(conn.token, conn);
         Ok(())
     }
+
+    fn shutdown(&mut self, event_loop: &mut EventLoop<Self>) {
+        let keys: Vec<Token> = self.conns.keys().cloned().collect();
+
+        for t in keys.iter() {
+            self.close_connection(event_loop, *t);
+        }
+
+        event_loop.shutdown();
+    }
+
+    fn close_connection(&mut self, event_loop: &mut EventLoop<Self>, token: Token) {
+        match self.conns.remove(&token) {
+            Some(conn) => {
+                conn.shutdown(event_loop).unwrap_or_else(|e| {
+                    println!("Error shutting down connection {:?}: {}", token, e);
+                })
+            },
+            None => {
+                println!("Could not find connection {:?}", token);
+            }
+        }
+    }
 }
 
 impl Handler for ServerHandler {
     type Timeout = usize;
-    type Message = ();
+    type Message = Notification;
 
-    fn readable(&mut self, event_loop: &mut EventLoop<Self>, token: Token, _: ReadHint) {
+    fn readable(&mut self, event_loop: &mut EventLoop<Self>, token: Token, hint: ReadHint) {
         match token {
             t if t == self.token => {
                 self.accept(event_loop).unwrap_or_else(|e| {
-                    println!("Error accepting connection: {}", e);
+                    println!("Error accepting connection: {} (hint: {:?})", e, hint);
                 });
             },
             t if self.conns.contains_key(&t) => {
                 let conn = self.conns.get_mut(&t).unwrap();
-                conn.readable(event_loop).unwrap_or_else(|e| {
+                conn.readable(event_loop, hint).unwrap_or_else(|e| {
                     println!("Error handling readable event for connection {:?}: {}", t, e);
                 });
             },
@@ -119,25 +142,13 @@ impl Handler for ServerHandler {
         }
     }
 
-    fn notify(&mut self, event_loop: &mut EventLoop<Self>, message: ()) {
+    fn notify(&mut self, event_loop: &mut EventLoop<Self>, message: Notification) {
         println!("Notify event: message = {:?}", message);
 
-        let keys: Vec<Token> = self.conns.keys().cloned().collect();
-
-        for t in keys.iter() {
-            match self.conns.remove(t) {
-                Some(conn) => {
-                    conn.shutdown(event_loop).unwrap_or_else(|e| {
-                        println!("Error shutting down connection {:?}: {}", t, e);
-                    })
-                },
-                None => {
-                    println!("Could not find connection {:?}", t);
-                }
-            }
+        match message {
+            Notification::Shutdown => self.shutdown(event_loop),
+            Notification::CloseConnection(token) => self.close_connection(event_loop, token),
         }
-
-        event_loop.shutdown();
     }
 
     fn timeout(&mut self, _: &mut EventLoop<Self>, timeout: usize) {
@@ -146,9 +157,25 @@ impl Handler for ServerHandler {
 
     fn interrupted(&mut self, event_loop: &mut EventLoop<Self>) {
         println!("Interrupted event.");
-        event_loop.channel().send(()).unwrap_or_else(|e| {
+        event_loop.channel().send(Notification::shutdown()).unwrap_or_else(|e| {
             println!("Error handling interrupted event by sending message: {:?}", e);
         });
+    }
+}
+
+#[derive(Debug)]
+pub enum Notification {
+    CloseConnection(Token),
+    Shutdown,
+}
+
+impl Notification {
+    pub fn close_connection(token: Token) -> Notification {
+        Notification::CloseConnection(token)
+    }
+
+    pub fn shutdown() -> Notification {
+        Notification::Shutdown
     }
 }
 
@@ -173,16 +200,20 @@ impl Connection {
         }
     }
 
-    fn readable<S: Handler>(&mut self, event_loop: &mut EventLoop<S>) -> Result<()> {
-        match self.sock.read(&mut self.in_buf) {
+    fn readable(&mut self, event_loop: &mut EventLoop<ServerHandler>, hint: ReadHint) -> Result<()> {
+        print!("Readable event on {:?} (hint: {:?}): ", self.token, hint);
+
+        let read_result = self.sock.read(&mut self.in_buf);
+
+        match read_result {
             Ok(Some(n)) => {
                 println!("Read {} bytes from {:?}", n, self.token);
             },
             Ok(None) => {
                 println!("No more bytes to read from {:?}", self.token);
             },
-            Err(e) => {
-                return Err(Error::from(e));
+            Err(_) => {
+                println!("(error)");
             }
         }
 
@@ -197,10 +228,14 @@ impl Connection {
             None => {}
         }
 
+        if hint.is_hup() {
+            try!(event_loop.channel().send(Notification::close_connection(self.token)));
+        }
+
         Ok(())
     }
 
-    fn writable<S: Handler>(&mut self, event_loop: &mut EventLoop<S>) -> Result<()> {
+    fn writable(&mut self, event_loop: &mut EventLoop<ServerHandler>) -> Result<()> {
         if Buf::has_remaining(&self.out_buf) {
             match self.sock.write(&mut self.out_buf) {
                 Ok(Some(n)) => {
@@ -221,7 +256,7 @@ impl Connection {
         Ok(())
     }
 
-    fn shutdown<S: Handler>(self, event_loop: &mut EventLoop<S>) -> Result<()> {
+    fn shutdown(self, event_loop: &mut EventLoop<ServerHandler>) -> Result<()> {
         use std::io::Write;
 
         println!("Shutting down {:?}", self.token);
@@ -252,6 +287,7 @@ impl Connection {
 #[derive(Debug)]
 pub enum Error {
     IoError(io::Error),
+    FullNotifyQueue,
     Other,
 }
 
@@ -259,6 +295,7 @@ impl error::Error for Error {
     fn description(&self) -> &str {
         match *self {
             Error::IoError(ref io_err) => io_err.description(),
+            Error::FullNotifyQueue => "Notification queue is full",
             Error::Other => "Other error",
         }
     }
@@ -268,6 +305,7 @@ impl Display for Error {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         match *self {
             Error::IoError(ref io_err) => write!(f, "{}", io_err),
+            Error::FullNotifyQueue => write!(f, "Notification queue is full"),
             Error::Other => write!(f, "Other error"),
         }
     }
@@ -276,6 +314,15 @@ impl Display for Error {
 impl From<io::Error> for Error {
     fn from(io_err: io::Error) -> Error {
         Error::IoError(io_err)
+    }
+}
+
+impl From<NotifyError<Notification>> for Error {
+    fn from(not_err: NotifyError<Notification>) -> Error {
+        match not_err {
+            NotifyError::Io(io_err) => Error::IoError(io_err),
+            NotifyError::Full(_) => Error::FullNotifyQueue,
+        }
     }
 }
 
@@ -325,7 +372,7 @@ mod tests {
             reader.read_line(&mut resp).unwrap();
             assert!(!resp.is_empty());
 
-            channel.send(()).unwrap();
+            channel.send(Notification::shutdown()).unwrap();
         });
 
         server.run(&mut handler).unwrap();
@@ -350,7 +397,7 @@ mod tests {
             assert!(!resp.is_empty());
 
 
-            channel.send(()).unwrap();
+            channel.send(Notification::shutdown()).unwrap();
         });
 
         server.run(&mut handler).unwrap();
