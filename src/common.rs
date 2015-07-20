6e3fd3e6776bff937cb1a3d044929555faa87645
Andrew Watts <ahwatts@gmail.com>
2015-07-20 16:43:56 -0400

    Remove now-unnecessary code. Add tests to the common stuff.

--- src/common.rs
+++ src/common.rs
@@ -4,35 +4,20 @@ use std::sync::{Arc, Mutex};
 #[derive(Debug)]
 pub struct Domain {
     name: String,
-    // classes: HashMap<String, Class>,
     files: HashMap<String, FileInfo>,
 }
 
 impl Domain {
     pub fn new(name: &str) -> Domain {
-        let rv = Domain {
+        Domain {
             name: name.to_string(),
-            // classes: HashMap::new(),
             files: HashMap::new(),
-        };
-        // let default_class_name = "default".to_string();
-        // Class::new("default", &rv).unwrap();
-        rv
+        }
     }
 
     pub fn name(&self) -> &str {
         &self.name
     }
-
-    // pub fn class(&self, name: &str) -> Option<&Class> {
-    //     self.classes.get(name).map(|c| c.deref())
-    // }
-    //
-    // pub fn classes(&self) -> &[&Class] {
-    //     let mut rv: Vec<&Class> = self.classes.values().map(|c| c.deref()).collect();
-    //     // rv.sort_by(|c1, c2| c1.name.cmp(&c2.name));
-    //     &rv
-    // }
 
     pub fn file(&self, key: &str) -> Option<&FileInfo> {
         self.files.get(key)
@@ -56,35 +41,11 @@ impl Domain {
     }
 }
 
-// #[derive(Debug)]
-// pub struct Class {
-//     name: String,
-// }
-
-// impl Class {
-//     pub fn new(name: &str, domain: &mut Domain) -> MogResult<Class> {
-//         if domain.classes.contains_key(name) {
-//             Err(MogError::DuplicateClass)
-//         } else {
-//             let class_name = name.to_string();
-//             let rv = Class { name: class_name.clone() };
-//             domain.classes.insert(class_name, rv);
-//             Ok(rv)
-//         }
-//     }
-
-//     pub fn name(&self) -> &str {
-//         &self.name
-//     }
-// }
-
 #[derive(Debug)]
 pub struct FileInfo {
     key: String,
     pub content: Option<Vec<u8>>,
     pub size: Option<usize>,
-    // domain_name: &'static str,
-    // class_name: &'static str,
 }
 
 impl FileInfo {
@@ -93,32 +54,12 @@ impl FileInfo {
             key: key.to_string(),
             content: None,
             size: None,
-            // domain_name: domain_name,
-            // class_name: class_name,
         })
-        // match domain.classes.get(class_name) {
-        //     Some(class) => Ok(FileInfo {
-        //         key: key.to_string(),
-        //         content: None,
-        //         size: None,
-        //         domain: domain_name,
-        //         class: class_name,
-        //     }),
-        //     None => Err(MogError::UnknownClass)
-        // }
     }
 
     pub fn key(&self) -> &str {
         &self.key
     }
-
-    // pub fn domain(&self) -> &Domain {
-    //     &self.domain
-    // }
-
-    // pub fn class(&self) -> &Class {
-    //     &self.class
-    // }
 }
 
 #[derive(Debug, Default)]
@@ -150,7 +91,7 @@ impl Backend {
 
 pub type SyncBackend = Arc<Mutex<Backend>>;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum MogError {
     DuplicateKey,
     DuplicateClass,
@@ -160,6 +101,151 @@ pub enum MogError {
 }
 
 pub type MogResult<T> = Result<T, MogError>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::test_support::*;
+
+    #[test]
+    fn test_create_domain() {
+        let domain = Domain::new("test_domain_2");
+        assert_eq!("test_domain_2", domain.name());
+        assert!(domain.files.is_empty());
+    }
+
+    #[test]
+    fn test_domain_get_file() {
+        let mut domain = domain_fixture();
+
+        {   // immutable, file present
+            let file = domain.file(TEST_KEY_1);
+            assert!(file.is_some());
+            assert_eq!(TEST_KEY_1, file.unwrap().key());
+        }
+
+        {   // immutable, file not present
+            let file2 = domain.file("test/key/3");
+            assert!(file2.is_none());
+        }
+
+        {   // mutable, file present
+            let file3 = domain.file_mut(TEST_KEY_1);
+            assert!(file3.is_some());
+            assert_eq!(TEST_KEY_1, file3.unwrap().key());
+        }
+
+        {   // mutable, file not present
+            let file4 = domain.file_mut("test/key/3");
+            assert!(file4.is_none());
+        }
+    }
+
+    #[test]
+    fn test_domain_get_mut_file() {
+        let mut domain = domain_fixture();
+        let new_content: Vec<u8> = b"Different content".iter().cloned().collect();
+
+        {   // Modify the content of the file.
+            let mut_file = domain.file_mut(TEST_KEY_1).unwrap();
+            mut_file.content = Some(new_content.clone());
+        }
+
+        {   // Pull it back out and make sure that it's the same.
+            let file = domain.file(TEST_KEY_1).unwrap();
+            assert_eq!(Some(new_content.clone()), file.content);
+        }
+    }
+
+    #[test]
+    fn test_domain_add_file() {
+        let mut domain = domain_fixture();
+        let new_key = "test/key/3";
+        let content: Vec<u8> = b"New file content".iter().cloned().collect();
+
+        {   // Add a new file to the domain.
+            let mut file = FileInfo::new(new_key).unwrap();
+            file.content = Some(content.clone());
+            file.size = Some(content.len());
+            domain.add_file(new_key, file).unwrap();
+        }
+
+        {   // Pull it back out and make sure it's the same.
+            let file = domain.file(new_key);
+            assert!(file.is_some());
+            let file = file.unwrap();
+            assert_eq!(new_key, file.key());
+            assert_eq!(Some(&content), file.content.as_ref());
+            assert_eq!(Some(&content.len()), file.size.as_ref());
+        }
+
+        {   // Try adding a duplicate key to the domain.
+            let file = FileInfo::new(TEST_KEY_1).unwrap();
+            let result = domain.add_file(TEST_KEY_1, file);
+            assert!(result.is_err());
+            assert_eq!(MogError::DuplicateKey, result.unwrap_err());
+        }
+    }
+
+    #[test]
+    fn test_domain_remove_file() {
+        let mut domain = domain_fixture();
+
+        {
+            let remove_result = domain.remove_file(TEST_KEY_2);
+            assert!(remove_result.is_some());
+            let removed = remove_result.unwrap();
+            assert_eq!(TEST_KEY_2, removed.key());
+        }
+
+        {
+            let get_result = domain.file(TEST_KEY_2);
+            assert!(get_result.is_none());
+        }
+
+        {
+            let remove_result_2 = domain.remove_file(TEST_KEY_2);
+            assert!(remove_result_2.is_none());
+        }
+    }
+
+    #[test]
+    fn backend_get_file() {
+        let mut backend = backend_fixture();
+
+        {   // immutable, file present
+            match backend.file(TEST_DOMAIN, TEST_KEY_1) {
+                Ok(Some(file)) => {
+                    assert_eq!(TEST_KEY_1, file.key());
+                },
+                v @ _ => panic!("Bad return for getting present file immutably: {:?}", v),
+            }
+        }
+
+        {   // immutable, file not present
+            match backend.file(TEST_DOMAIN, "test/key/3") {
+                Ok(None) => {},
+                v @ _ => panic!("Bad return for getting missing file immutably: {:?}", v),
+            }
+        }
+
+        {   // mutable, file present
+            match backend.file_mut(TEST_DOMAIN, TEST_KEY_1) {
+                Ok(Some(file)) => {
+                    assert_eq!(TEST_KEY_1, file.key());
+                },
+                v @ _ => panic!("Bad return for getting present file mutably: {:?}", v),
+            }
+        }
+
+        {   // mutable, file not present
+            match backend.file_mut(TEST_DOMAIN, "test/key/3") {
+                Ok(None) => {},
+                v @ _ => panic!("Bad return for getting missing file mutably: {:?}", v),
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 pub mod test_support {
@@ -177,29 +263,35 @@ pub mod test_support {
 
     pub static TEST_KEY_2: &'static str = "test/key/2";
 
-    pub fn backend_fixture() -> Backend {
+    pub fn domain_fixture() -> Domain {
         let mut domain = Domain::new(TEST_DOMAIN);
+        domain.files.insert(TEST_KEY_1.to_string(), file_1_fixture());
+        domain.files.insert(TEST_KEY_2.to_string(), file_2_fixture());
+        domain
+    }
 
-        domain.files.insert(
-            TEST_KEY_1.to_string(),
-            FileInfo {
-                key: TEST_KEY_1.to_string(),
-                content: Some(Vec::from(TEST_CONTENT_1)),
-                size: Some(TEST_CONTENT_1.len()),
-            });
+    pub fn file_1_fixture() -> FileInfo {
+        FileInfo {
+            key: TEST_KEY_1.to_string(),
+            content: Some(Vec::from(TEST_CONTENT_1)),
+            size: Some(TEST_CONTENT_1.len()),
+        }
+    }
 
-        domain.files.insert(
-            TEST_KEY_2.to_string(),
-            FileInfo {
-                key: TEST_KEY_2.to_string(),
-                content: None,
-                size: None,
-            });
+    pub fn file_2_fixture() -> FileInfo {
+        FileInfo {
+            key: TEST_KEY_2.to_string(),
+            content: None,
+            size: None,
+        }
+    }
 
+    pub fn backend_fixture() -> Backend {
         let mut backend = Backend {
             domains: HashMap::new(),
         };
-        backend.domains.insert(domain.name.clone(), domain);
+        let domain = domain_fixture();
+        backend.domains.insert(domain.name().to_string(), domain);
         backend
     }
 
