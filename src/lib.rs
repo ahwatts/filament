ebcecf89becad176d475d26c04bcc4a667efa62a
Andrew Watts <ahwatts@gmail.com>
2015-07-21 15:42:08 -0400

    We don't need the regex lib any more.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ extern crate url;
 #[cfg(feature = "evented")] extern crate mio;
 #[cfg(feature = "evented")] extern crate threadpool;
 
-#[cfg(test)] extern crate regex;
 #[cfg(test)] #[macro_use] extern crate matches;
 
 #[macro_use] extern crate lazy_static;
