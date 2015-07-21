b2f172a37aa254b9d2103bbad6149074171be00c
Andrew Watts <ahwatts@gmail.com>
2015-07-21 16:32:37 -0400

    Restore the storage server handling code.

--- src/storage/iron.rs
+++ src/storage/iron.rs
@@ -1,58 +1,63 @@
-use iron::{Handler, IronResult, Request, Response};
+use iron::{Handler, IronError, IronResult, Request, Response};
+use iron::headers;
+use iron::method::Method;
+use iron::modifier::Set;
+use iron::modifiers::Header;
 use iron::status::Status;
+use std::error::Error;
+use std::ops::Deref;
 use super::Storage;
+use super::super::error::MogError;
 
-pub struct StorageHandler;
-// {
-//     store: Storage,
-// }
+pub struct StorageHandler {
+    store: Storage,
+}
 
 impl StorageHandler {
-    pub fn new(_: Storage) -> StorageHandler {
-        StorageHandler
-        // {
-        //     store: storage,
-        // }
+    pub fn new(storage: Storage) -> StorageHandler {
+        StorageHandler {
+            store: storage,
+        }
     }
 
-    // fn handle_get(&self, request: &Request, domain: &str, key: &str) -> IronResult<Response> {
-    //     let mut content = vec![];
-    //     match self.store.get_content(domain, key, &mut content) {
-    //         Ok(_) => {},
-    //         Err(StorageError::UnknownKey) => {
-    //             return Ok(Response::with((Status::NotFound, "Unknown key.\n")));
-    //         },
-    //         Err(StorageError::NoContent) => {
-    //             return Ok(Response::with((Status::NotFound, "No content for key.\n")));
-    //         },
-    //         Err(e) => {
-    //             let modifier = (Status::InternalServerError, format!("{}\n", e.description()));
-    //             return Err(IronError::new(e, modifier));
-    //         },
-    //     };
+    fn handle_get(&self, request: &Request, domain: &str, key: &str) -> IronResult<Response> {
+        let mut content = vec![];
+        match self.store.get_content(domain, key, &mut content) {
+            Ok(_) => {},
+            Err(MogError::UnknownKey(ref k)) => {
+                return Ok(Response::with((Status::NotFound, format!("Unknown key: {:?}\n", k))));
+            },
+            Err(MogError::NoContent(ref k)) => {
+                return Ok(Response::with((Status::NotFound, format!("No content for key: {:?}\n", k))));
+            },
+            Err(e) => {
+                let modifier = (Status::InternalServerError, format!("{}\n", e.description()));
+                return Err(IronError::new(e, modifier));
+            },
+        };
 
-    //     let mut response = Response::with((Status::Ok,));
-    //     response = response.set(Header(headers::ContentLength(content.len() as u64)));
+        let mut response = Response::with((Status::Ok,));
+        response = response.set(Header(headers::ContentLength(content.len() as u64)));
 
-    //     if request.method == Method::Get {
-    //         response = response.set(content);
-    //     }
+        if request.method == Method::Get {
+            response = response.set(content);
+        }
 
-    //     Ok(response)
-    // }
+        Ok(response)
+    }
 
-    // fn handle_put(&self, request: &mut Request, domain: &str, key: &str) -> IronResult<Response> {
-    //     match self.store.store_content(domain, key, &mut request.body) {
-    //         Ok(_) => Ok(Response::with((Status::Ok,))),
-    //         Err(StorageError::UnknownKey) => {
-    //             return Ok(Response::with((Status::NotFound, "Unknown key.\n")));
-    //         },
-    //         Err(e) => {
-    //             let modifier = (Status::InternalServerError, format!("{}\n", e.description()));
-    //             return Err(IronError::new(e, modifier));
-    //         },
-    //     }
-    // }
+    fn handle_put(&self, request: &mut Request, domain: &str, key: &str) -> IronResult<Response> {
+        match self.store.store_content(domain, key, &mut request.body) {
+            Ok(_) => Ok(Response::with((Status::Ok,))),
+            Err(MogError::UnknownKey(ref k)) => {
+                return Ok(Response::with((Status::NotFound, format!("Unknown key: {:?}\n", k))));
+            },
+            Err(e) => {
+                let modifier = (Status::InternalServerError, format!("{}\n", e.description()));
+                return Err(IronError::new(e, modifier));
+            },
+        }
+    }
 }
 
 impl Handler for StorageHandler {
@@ -64,11 +69,14 @@ impl Handler for StorageHandler {
         }
 
         let (domain, key) = dk.unwrap();
-        info!("Storage request: {:?} domain = {} key = {} from {:?}", request.method, domain, key, request.remote_addr);
+        info!("Storage request: {:?} domain = {} key = {} (body = {} bytes) from {:?}",
+              request.method, domain, key,
+              request.headers.get::<headers::ContentLength>().map(|h| h.deref()).unwrap_or(&0),
+              request.remote_addr);
 
         match request.method {
-            // Method::Get | Method::Head => self.handle_get(request, &domain, &key),
-            // Method::Put => self.handle_put(request, &domain, &key),
+            Method::Get | Method::Head => self.handle_get(request, &domain, &key),
+            Method::Put => self.handle_put(request, &domain, &key),
             _ => Ok(Response::with((Status::BadRequest, "Unknown request type.\n"))),
         }
     }
